a7790f704ed593fa01415bdb5e8b1aeaa0a891ae
starlord <starlord@xhiftcorp.com>
2019-04-04 18:47:20 +0900

    mail 전송 관련

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
-require("dotenv").config()
+import dotenv from "dotenv"
+import path from "path";
+dotenv.config({path: path.resolve(__dirname, ".env")});
+
 import { GraphQLServer } from "graphql-yoga";
 import logger from "morgan";
 import schema from "./schema";
+
+// console.log(__dirname)
 
 const PORT = process.env.PORT || 4000;
 
