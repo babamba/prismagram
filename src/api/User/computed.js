4a2a3c0782959dd4698167c27ccfaeaf1134ff94
starlord <starlord@xhiftcorp.com>
2019-04-05 17:55:18 +0900

    computed model

---
+++ src/api/User/computed.js
@@ -0,0 +1,60 @@
+import { prisma } from "../../../generated/prisma-client";
+
+ //db필드에는 없는  computed 혹은 custom field(임의) 
+// resolver 에 있으니까 custom resolver
+// parent 는 resolver를 call하는 resolver
+// 여기서의 fullname의 parent는 위 resolver의 리턴된 user 
+// schema.js 에서 resolver들을 하나로 merge 하고있기때문에 다른 api에서도 fullName을 호출할 수 있다.
+
+export default {
+     User:{
+          //fullName:(_, args, { request }) => {
+          //parent 로 부터 모든걸 받을 수있다.
+          fullName: (parent) => {
+               console.log("parent", parent)
+               //console.log(request);
+               return `${parent.firstName} ${parent.lastName}` ;
+          },
+          // 내가 요청한 아이디를 팔로잉하고있는지
+          isFollowing: async(parent, _ , {request}) => {
+               console.log("parentId : ", parentId)
+
+               // 요청한 유저  
+               const { user } = request;
+
+               // id
+               const { id: parentId } = parent;
+
+               try {
+                    //const exist = await prisma.$exists.user({
+                    return prisma.$exists.user({
+                         AND: [
+                              {id: user.id},
+                              {following_some:{
+                                   id: parentId
+                              }}  //팔로잉 리스트에 있는 id 중 내 id가 있는지
+                         ]
+                    })
+     
+                    // if(exist){
+                    //      return true;
+                    // }else{
+                    //      return false;
+                    // }
+               } catch (error) {
+                    console.log(error)
+                    return false;
+               }
+               
+               
+          },
+          // 요청한 사람이 같은 사람인지 
+          // ( 내가 내 프로필을 보는건지 남이 내프로필을 보는건지  프로필 수정 버튼노출을 위해?)
+          isSelf:(parent , _ , {request}) => {
+               const { user } = request;
+               const { id: parentId } = parent;
+               return user.id === parentId; 
+          }
+
+     }
+}
--- src/api/User/me/me.js
+++ src/api/User/me/me.js
@@ -4,6 +4,7 @@ import { USER_FRAGMENT } from "../../../fragments"
 export default {
      Query : {
           me: async(_, args , {request, isAuthenticated}) => {
+               //console.log(" me parent " , _)
                isAuthenticated(request);
                const { user } = request;
                //return prisma.user({id: user.id}).$fragment(USER_FRAGMENT);
@@ -18,5 +19,21 @@ export default {
                     posts
                };
           }
-     }
+     },
+     //db필드에는 없는  computed 혹은 custom field(임의) 
+     // resolver 에 있으니까 custom resolver
+
+     // parent 는 resolver를 call하는 resolver
+     // 여기서의 fullname의 parent는 위 resolver의 리턴된 user 
+
+     // schema.js 에서 resolver들을 하나로 merge 하고있기때문에 다른 api에서도 fullName을 호출할 수 있다.
+     // User:{
+     //      //fullName:(_, args, { request }) => {
+     //      //parent 로 부터 모든걸 받을 수있다.
+     //      fullName: (parent) => {
+     //           console.log("parent", parent)
+     //           //console.log(request);
+     //           return `${parent.firstName} ${parent.lastName}` ;
+     //      }
+     // },
 };
