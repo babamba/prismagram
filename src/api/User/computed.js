96c88572322733d50deba4c45293f919cdab8832
starlord <starlord@xhiftcorp.com>
2019-04-08 11:07:49 +0900

    좋아요 유무

--- src/api/User/computed.js
+++ src/api/User/computed.js
@@ -55,6 +55,26 @@ export default {
                const { id: parentId } = parent;
                return user.id === parentId; 
           }
-
+     },
+     Post:{
+          isLiked : (parent, _ , { request }) => {
+               const { user } = request;
+               const { id } = parent;
+               return prisma.$exists.like({
+                    AND:[
+                         {
+                              user:{
+                                   id: user.id
+                              }
+                         },
+                         {
+                              post:{
+                                   id
+                              }
+                         }
+                    ]
+               })
+          }
      }
+
 }
