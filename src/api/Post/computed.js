bf9b4bf5e9d18a1acbf81b9b969c88886b09f57f
starlord <starlord@xhiftcorp.com>
2019-04-08 12:12:29 +0900

    upload 및 fragment

---
+++ src/api/Post/computed.js
@@ -0,0 +1,35 @@
+import { prisma } from "../../../generated/prisma-client";
+
+ //db필드에는 없는  computed 혹은 custom field(임의) 
+// resolver 에 있으니까 custom resolver
+// parent 는 resolver를 call하는 resolver
+// 여기서의 fullname의 parent는 위 resolver의 리턴된 user 
+// schema.js 에서 resolver들을 하나로 merge 하고있기때문에 다른 api에서도 fullName을 호출할 수 있다.
+
+export default {
+     Post:{
+          isLiked : (parent, _ , { request }) => {
+               const { user } = request;
+               const { id } = parent;
+               return prisma.$exists.like({
+                    AND:[
+                         {
+                              user:{
+                                   id: user.id
+                              }
+                         },
+                         {
+                              post:{
+                                   id
+                              }
+                         }
+                    ]
+               })
+          },
+          likeCount:(parent) => 
+               prisma
+               .likesConnection({
+                    where:{ post :{ id :parent.id } }
+               }).aggregate().count()
+     }
+}
--- src/fragments.js
+++ src/fragments.js
@@ -1,9 +1,41 @@
 
 export const COMMENT_FRAGMENT = `
-     fragment CommentPars on Comment{
           id
           text
           user {
+               ${USER_FRAGMENT}
+          }
+`
+
+export const USER_FRAGMENT = `
+          id
+          username
+`
+
+export const FILE_FRAGMENT = `
+          id
+          url
+`
+
+export const FULL_POST_FRAGMENT = `
+     fragment PostParts on Post{ 
+          id
+          location
+          caption
+          files{
+               id
+               url
+          }
+          comments{
+               id
+               text
+               user{
+                    id
+                    username
+               }
+          }
+          user{
+               id
                username
           }
      }
